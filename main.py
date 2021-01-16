f29c97ae1886d195721e5135bbe526904409863f
Angela.Duran <angelad2000@gmail.com>
2021-01-16 07:18:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+with open('simple.html') as html_file:
+  soup = BeautifulSoup(html_file, 'lxml')
+
+#Cleans up HTML with indendation
+#print(soup.prettify())
+
+match = soup.title.text #Getting only the text tag
+#print(match)
+
+#article = soup.find('div', class_='article')
+#finds only first article
+
+for article in soup.find_all('div', class_='article'): #returns a list
+
+  headline = article.h2.a.text
+  print(headline)
+
+  summary = article.p.text #have to unrap-ish
+  print(summary)
+
+  print()
